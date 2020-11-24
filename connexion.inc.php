aeb7d054038716de1d427867ba1bac57b0ede84b
Damien <dchalot14@gmail.com>
2020-11-24 20:26:00 +0100

    connection bdd et inscription 1

---
+++ connexion.inc.php
@@ -0,0 +1,18 @@
+<?php
+function connect(){
+    $user = 'root';
+    $pass = 'root';
+    $db_name = 'matchmygame';
+    try {
+        return $bdd = new PDO('mysql:host=localhost;matchmygame='.$db_name, $user, $pass);
+    } 
+    catch (PDOException $e) {
+        //print "Erreur !: " . $e->getMessage() . "<br/>";
+        die("Erreur de connexion à la base de données !");
+    }
+}
+function close(){
+    global $bdd;
+    $dbh = null;
+}
+?>
