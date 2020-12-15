7c571e11bf1e3ab69712c6eb0452ea59ed1d1da2
Damien <dchalot14@gmail.com>
2020-12-15 09:25:52 +0100

    modif mention légales et confidentialité

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
 	<div id="footer"> 
 
-			<a href="légales.html">Mentions légales </a>
+			<a href="legales.html">Mentions légales </a>
 			<a href="confidentialité.html"s>Politique de confidentialité</a>
 		
 	</div> 
