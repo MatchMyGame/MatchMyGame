6940702dddeea99f4308d4ad9d241aaf52ecb3d5
Damien <dchalot14@gmail.com>
2020-12-22 11:18:22 +0100

    formulaire mon compte

--- compte.php
+++ compte.php
@@ -42,6 +42,69 @@ if(!(isset($_SESSION['idUser']) AND !empty($_SESSION['idUser']))){
 			} 
 			?>
 
+			<form class="inscription"  action="" method="post">
+				<fieldset>
+				    <?php 
+				    if (isset($error)){
+				          echo $error;
+				    
+				    } 
+				    if (isset($success)){
+				          echo $success;
+				    
+				    }
+				    ?>
+				    <p><i>Vous pouvez modifier votre mot de passe, modifier les jeux auquels vous jouez ainsi que les plateformes sur lesquelles vous jouez</i></p>
+				    
+
+				      <div class="form">
+				        <label>Mot de passe actuel</label>
+				        <input class="form-input" placeholder="Mot de passe" name="password" type="password" value="">
+				      </div>
+
+				      <div class="form">
+				        <label>Nouveaux mot de passe</label>
+				        <input class="form-input" placeholder="Confirmer le mot de passe" name="password1" type="password" value="">
+				      </div> 
+
+				      <div class="form">
+				        <label>Confirmez votre nouveau mot de passe</label>
+				        <input class="form-input" placeholder="Confirmer le mot de passe" name="password2" type="password" value="">
+				      </div> 
+
+				      <div>
+				          <label for="plateforme">Renseigner vos plateformes</label> <br>
+				          
+				            <label for="PS4"><input id="fortnite" type="checkbox" name="plateforme[]" value="PS4"> PS4</label>
+				            <label for="PC"><input id="LOL" type="checkbox" name="plateforme[]" value="PC"> PC</label>
+				            <label for="XBOX ONE"><input id="COD" type="checkbox" name="plateforme[]" value="XBOX ONE"> XBOX ONE</label>
+				            <label for="SWITCH"><input id="MH" type="checkbox" name="plateforme[]" value="SWITCH">SWITCH</label>
+				            <label for="PS5"><input id="OV" type="checkbox" name="plateforme[]" value="PS5"> PS5</label>
+				            <label for="XBOX SERIE X"><input id="RK" type="checkbox" name="plateforme[]" value="XBOX SERIE X"> XBOX SERIE X</label>
+				            <label for="XBOX SERIE S"><input id="Among us" type="checkbox" name="plateforme[]" value="XBOX SERIE S"> XBOX SERIE S</label>
+				          
+				      </div>
+				    
+				    <div>
+				      <legend>Choissisez vos jeux</legend>
+				      <label for="fortnite"><input id="fortnite" type="checkbox" name="jeux[]" value="Fortnite"> Fortnite</label>
+				      <label for="LOL"><input id="LOL" type="checkbox" name="jeux[]" value="LOL"> LEAGUE Of LEGEND</label>
+				      <label for="COD"><input id="COD" type="checkbox" name="jeux[]" value="COD"> CALL OF DUTY</label>
+				      <label for="MH"><input id="R6" type="checkbox" name="jeux[]" value="R6"> Rainbow six siege</label>
+				      <label for="OV"><input id="OV" type="checkbox" name="jeux[]" value="OV"> OVERWATCH</label>
+				      <label for="RK"><input id="RK" type="checkbox" name="jeux[]" value="RL"> ROCKET LEAGUE</label>
+				      <label for="Among US"><input id="Among us" type="checkbox" name="jeux[]" value="among"> AMONG US</label>
+				      <label for="FIFA"><input id="FIFA" type="checkbox" name="jeux[]" value="FIFA"> FIFA 21</label>
+				    </div>
+				    
+				    <input type="submit" name="changement" value="Changer mes informations">
+
+
+				  </fieldset>
+
+				
+			</form>
+
 		<?php include('footer.php') ?>  
 		</body>	
 	</div>
